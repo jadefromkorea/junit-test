787f24eddd41614f344913e60236bff36792700c
이종현 <jadefromkorea@gmail.com>
2021-11-21 15:42:04 +0900

    Initial commit

---
+++ junit-test/iloveyouboss_01/test/iloveyouboss/ScoreCollectionTest.java
@@ -0,0 +1,17 @@
+package iloveyouboss;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScoreCollectionTest {
+
+    @Test
+    void add() {
+//        fail("실패 테스트!!!");
+    }
+
+    @Test
+    void arithmeticMean() {
+    }
+}
